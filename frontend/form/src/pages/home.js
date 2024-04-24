2caab064804606bffc0d6996d242c35a9574c914
nabin2004 <olinabin2004@gmail.com>
2024-04-24 22:43:19 +0545

    Added missing link functionality to the button for improved user experience.

--- frontend/form/src/pages/home.js
+++ frontend/form/src/pages/home.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import cairnepal from '../images/fulllogo.png';
 import React from 'react';
 
@@ -7,12 +8,15 @@ const Home = () => {
             <h1 className='projectName'>NCHLOD</h1>
 
             <div className="mainButtons">
+                
                 <div className="mainElement">
+                <Link to="/fields">
                     <button className="contributor">
                         <span class="material-symbols-outlined">
                             bloodtype
                         </span>
                     </button>
+                    </Link>
                     <label>CONTRIBUTOR</label>
                 </div>
 
