d356b01b0245c4389c81d006b06f4233e9df9484
nabin2004 <olinabin2004@gmail.com>
2024-07-21 22:53:05 +0545

    Updated link to API

--- docroot/frontend/src/components/table/index.jsx
+++ docroot/frontend/src/components/table/index.jsx
@@ -15,7 +15,7 @@ const WebformSubmissionTable = ({ uuids }) => {
       try {
         const responses = await Promise.all(
           uuids.map(uuid =>
-            axios.get(`https://nchlod.ddev.site/webform_rest/heritage_graph/submission/${uuid}`)
+            axios.get(`https://nchlod.ddev.site/webform/nchlod1${uuid}`)
           )
         );
         const data = responses.map(response => response.data);
